923589babedee82e73eb3f41ad27ea1d30f6f704
isaced <isaced@163.com>
2016-12-01 12:07:45 +0800

    update vapor v1.2.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "NSPress",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1)
     ],
     exclude: [
